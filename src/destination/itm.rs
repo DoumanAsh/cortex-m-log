8145bbdb754278b1efae08f96b21b2c6b6ef83e5
Scott Mabin <MabezDev@gmail.com>
2019-01-29 16:04:44 +0000

    ITM/SYNC:
    	Implement cortex-m mutex around itm, maying it Sync

--- src/destination/itm.rs
+++ src/destination/itm.rs
@@ -1,33 +1,42 @@
 //! ITM module
 
-use cortex_m::{itm};
+use cortex_m::{itm, interrupt::Mutex};
 use core::fmt;
+
+use core::cell::RefCell;
 
 /// ITM based destination
 pub struct Itm {
-    inner: cortex_m::peripheral::ITM,
+    inner: Mutex<RefCell<cortex_m::peripheral::ITM>>,
 }
 
 impl Itm {
     /// Creates new instance by taking ownership of ITM register block
     pub fn new(itm: cortex_m::peripheral::ITM) -> Self {
-        Self { inner: itm }
+        Self { inner: Mutex::new(RefCell::new(itm)) }
     }
 }
 
 impl fmt::Write for Itm {
     #[inline]
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        let stim = &mut self.inner.stim[0];
-        itm::write_str(stim, s);
-
+        cortex_m::interrupt::free(|cs| {
+            if let Some(mut itm) = self.inner.borrow(cs).try_borrow_mut().ok() {
+                let stim = &mut itm.stim[0];
+                itm::write_str(stim, s);
+            }
+        });
         Ok(())
     }
 
     #[inline]
     fn write_fmt(&mut self, args: fmt::Arguments) -> fmt::Result {
-        let stim = &mut self.inner.stim[0];
-        itm::write_fmt(stim, args);
+        cortex_m::interrupt::free(|cs| {
+            if let Some(mut itm) = self.inner.borrow(cs).try_borrow_mut().ok() {
+                let stim = &mut itm.stim[0];
+                itm::write_fmt(stim, args);
+            }
+        });
 
         Ok(())
     }
