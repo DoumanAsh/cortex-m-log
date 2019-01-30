0375e74262a175c567eab4ffe5f14ac6dd2b2ec8
Douman <douman@gmx.se>
2019-01-30 18:35:27 +0300

    Add ItmSync

--- src/printer/itm.rs
+++ src/printer/itm.rs
@@ -1,11 +1,12 @@
 //! Itm module
 
-extern crate cortex_m;
+use cortex_m::interrupt::Mutex;
 
 use crate::destination;
 use crate::modes::InterruptModer;
 
 use core::marker::PhantomData;
+use core::fmt::{self, Write};
 
 /// ITM backed printer
 pub struct Itm<M: InterruptModer> {
@@ -30,7 +31,55 @@ impl<Mode: InterruptModer> super::Printer for Itm<Mode> {
     }
 }
 
+/// ITM backed printer with `Sync`
+pub struct ItmSync<M: InterruptModer> {
+    inner: destination::Itm,
+    lock: Mutex<()>,
+    _mod: PhantomData<M>,
+}
+
+impl<M: InterruptModer> ItmSync<M> {
+    ///Constructs new instance
+    pub fn new(itm: destination::Itm) -> Self {
+        Self { inner: itm, lock: Mutex::new(()), _mod: PhantomData }
+    }
+}
+
+impl<Mode: InterruptModer> super::Printer for ItmSync<Mode> {
+    type W = destination::Itm;
+    type M = Mode;
+
+    #[inline]
+    fn destination(&mut self) -> &mut Self::W {
+        &mut self.inner
+    }
+
+    #[inline]
+    ///Prints formatted output to destination
+    fn print(&mut self, args: fmt::Arguments) {
+        cortex_m::interrupt::free(|cs| {
+            let _lock = self.lock.borrow(cs);
+            let _ = self.inner.write_fmt(args);
+        });
+    }
+
+    #[inline]
+    ///Prints formatted output to destination with newline
+    fn println(&mut self, args: fmt::Arguments) {
+        cortex_m::interrupt::free(|cs| {
+            let _lock = self.lock.borrow(cs);
+            let _ = self.inner.write_fmt(args);
+            let _ = self.inner.write_str("\n");
+        });
+    }
+}
+
+unsafe impl<Mode: InterruptModer> Sync for ItmSync<Mode> {
+}
+
 /// Alias for Interrupt free Printer
 pub type InterruptFree = Itm<crate::modes::InterruptFree>;
 /// Alias for Printer without control over interrupts
 pub type InterruptOk = Itm<crate::modes::InterruptOk>;
+/// Alias for Synced Itm.
+pub type InterruptSync = ItmSync<crate::modes::InterruptFree>;
